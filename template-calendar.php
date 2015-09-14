189cfcaf7ae8af013de7a1df916a392f6d46a9e8
ksascomm <ksasweb@jhu.edu>
2015-09-14 16:41:23 -0400

    events calendar fix

--- template-calendar.php
+++ template-calendar.php
@@ -12,9 +12,17 @@ Template Name: Calendar
 				<?php the_content(); ?>
 			<?php endwhile; endif; ?>
 			
-			<!-- /************Calendar display**************/ -->	
-				<div class="row" id="calendar_container"></div>
-
+			<!-- /*****Calendar display for desktop then mobile*****/ -->
+				<?php if(is_mobile()) {  ?>	
+					<div class="row">
+						<a href="<?php echo get_site_url() . '/calendar/krieger_all' ; ?>">View our Events Calendar</a>
+					</div>
+				<?php } else { ?>
+					<div class="hide-for-small-only" id="calendar_container"></div>
+					<div class="show-for-small-only">
+						<a href="<?php echo get_site_url() . '/calendar/krieger_all' ; ?>">View our Events Calendar</a>
+					</div>
+				<?php } ?>
 		</section>
 	</div>
 </div> 
