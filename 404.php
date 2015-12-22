b66d11c310124863d3662a44aaf015acab346054
ksascomm <ksasweb@jhu.edu>
2015-12-22 13:45:31 -0500

    format changes

--- 404.php
+++ 404.php
@@ -2,15 +2,14 @@
 
 <div class="row wrapper radius10">
 	<section class="small-12 columns no-gutter" role="main-content">
-			<h2>Whoops...</h2>
-			<p>This page does not exist.  Try searching</p>
-			       <form class="search-form" action="<?php echo site_url('/search'); ?>" method="get">
-                    <fieldset>
-                        <input type="text" class="input-text" name="q" />
-                        <input type="submit" class="button blue_bg" value="Search" />
-                    </fieldset>
-       </form>        
-
+		<h2>Whoops...</h2>
+		<p>This page does not exist.  Try searching</p>
+		    <form class="search-form" action="<?php echo site_url('/search'); ?>" method="get">
+                <fieldset>
+                    <input type="text" class="input-text" name="q" />
+                    <input type="submit" class="button blue_bg" value="Search" />
+                </fieldset>
+   			</form>        
 	</section>
 </div>
 
