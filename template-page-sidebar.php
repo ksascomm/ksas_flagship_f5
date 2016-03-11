b7a051d1812b5aa3e1bf013e1f897179aa8ac8f6
ksascomm <ksasweb@jhu.edu>
2016-03-11 16:52:52 -0500

    modified sidebar page template

--- template-page-sidebar.php
+++ template-page-sidebar.php
@@ -6,27 +6,24 @@ Template Name: Page with Sidebar
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <div class="row sidebar_bg radius10" id="opp">
-	<div class="small-12 medium-8 columns wrapper radius-left offset-topgutter">		
-		<section class="content">
-				<h1><?php the_title();?></h1>
-					<?php if ( has_post_thumbnail()) { ?> 
-						<div class="photo-page-left floatleft small-12 medium-7 columns">
-							<?php the_post_thumbnail('full',array('class'	=> "radius-topleft")); ?>
-						</div>
-					<?php } ?>
-				<?php the_content(); ?>
-		</section>
-	</div>	<!-- End main content (left) section -->
+	<main class="small-12 medium-8 columns wrapper radius-left offset-topgutter" role="main">		
+		<h1><?php the_title();?></h1>
+			<?php if ( has_post_thumbnail()) { ?> 
+				<div class="photo-page-left floatleft small-12 medium-7 columns">
+					<?php the_post_thumbnail('full',array('class'	=> "radius-topleft")); ?>
+				</div>
+			<?php } ?>
+		<?php the_content(); ?>
+	</main>	<!-- End main content (left) section -->
 <?php endwhile; endif; ?>	
 	
-	<div class="small-12 medium-4 columns" id="sidebar"> <!-- Begin Sidebar -->
+	<aside class="small-12 medium-4 columns" id="sidebar" role="complementary"> <!-- Begin Sidebar -->
 		<div class="row">
 		<!-- Page Specific Sidebar -->
 		<?php if ( get_post_meta($post->ID, 'ecpt_page_sidebar', true) ) { 
 				echo apply_filters('the_content', get_post_meta($post->ID, 'ecpt_page_sidebar', true));
 			} ?>
 		</div> <!--End Dept Nav Links -->
-		</div> 
-	</div> <!-- End Sidebar -->
+	</aside> <!-- End Sidebar -->
 </div> <!-- End #landing -->
 <?php get_footer(); ?>
