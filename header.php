96033da96f58fe3ac218dab60e541112f1417d3c
ksascomm <ksasweb@jhu.edu>
2016-05-12 09:22:55 -0400

    last modified date in header

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="date" content="<?php the_modified_date(); ?>" />
   <title><?php create_page_title(); ?></title>
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/assets/images/favicon.ico" />
   <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri() ?>/assets/images/apple-touch-icon-144x144-precomposed.png" />
@@ -57,6 +58,7 @@ if( is_page() ) {
 	
 		<div id="desktop-nav">
 			<?php get_template_part( 'parts', 'offcanvas' ); ?>
+
 			<div class="row hide-for-print" role="navigation">
 				<?php wp_nav_menu( array( 
 					'theme_location' => 'main_nav', 
