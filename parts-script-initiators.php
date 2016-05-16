f7401105c15060f14b856a086b47650e2dcd64d9
ksascomm <ksasweb@jhu.edu>
2016-05-16 16:17:08 -0400

    smart pixel addition request from annual giving

--- parts-script-initiators.php
+++ parts-script-initiators.php
@@ -7,6 +7,27 @@ For production environment search and replace javascripts/ for javascripts/min.
   <script src="<?php echo get_template_directory_uri() ?>/assets/js/vendor/app.js"></script>
   <script src="<?php echo get_template_directory_uri() ?>/assets/js/vendor/offcanvas.js"></script>
 
+<script type="text/javascript">
+    adroll_adv_id = "ICTKBDRQ3BEGXMZ377ACAU";
+    adroll_pix_id = "SZ4TXYN3R5HP3DV7VMTSYQ";
+    (function () {
+        var _onload = function(){
+            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+            var scr = document.createElement("script");
+            var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+            scr.setAttribute('async', 'true');
+            scr.type = "text/javascript";
+            scr.src = host + "/j/roundtrip.js";
+            ((document.getElementsByTagName('head') || [null])[0] ||
+                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+        };
+        if (window.addEventListener) {window.addEventListener('load', _onload, false);}
+        else {window.attachEvent('onload', _onload)}
+    }());
+</script>
+
+
 	<script>
 	var $x = jQuery.noConflict();
 		$x('#quicklinks ul.flyout li a').on('click', function() {
