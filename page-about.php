3f0dc670d336f7c91ef1469c151149c5af4ee7f6
ksascomm <ksasweb@jhu.edu>
2016-05-24 12:08:01 -0400

    no-lazy class added to slider

--- page-about.php
+++ page-about.php
@@ -7,7 +7,7 @@
 				<p><?php the_content(); ?></p>
 			<?php endwhile; endif; ?>
 		</div>
-		<aside class="small-12 medium-6 columns" role="complementary">	
+		<aside class="small-12 medium-6 columns no-lazy " role="complementary">	
 			<ul id="slider" data-orbit data-options="animation_speed:2000; timer:true; slide_number: false; timer_speed:3000; navigation_arrows:false; bullets:false;" class="no-gutter photo-page-right no-lazy">
 				<?php if ( false === ( $by_the_numbers_query = get_transient( 'by_the_numbers_query' ) ) ) {
 			// It wasn't there, so regenerate the data and save the transient
@@ -19,9 +19,9 @@
 						)); 
 				set_transient( 'by_the_numbers_query', $by_the_numbers_query, 2592000 ); }
 					if ( $by_the_numbers_query->have_posts() ) : while ( $by_the_numbers_query->have_posts() ) : $by_the_numbers_query->the_post(); ?>
-				<div class="number">
+				<div class="number no-lazy">
 						<?php the_post_thumbnail('full',array( 'class'	=> "radius-topright no-lazy")); ?>
-						<summary class="<?php echo get_post_meta($post->ID, 'bg_color', true); ?>">
+						<summary class="<?php echo get_post_meta($post->ID, 'bg_color', true); ?> no-lazy">
 							<h3 class="no-margin white bold"><?php the_title(); ?></h3>
 							<h5 class="white bold no-margin"><?php echo get_the_content(); ?></h5>
 						</summary>
