8e9211195722c367e00319b30e9cbf1030c58613
ksascomm <ksasweb@jhu.edu>
2016-10-03 14:35:10 -0400

    move modernizr to head; defer 3rd party plugin js scripts

--- assets/functions/enqueue-scripts.php
+++ assets/functions/enqueue-scripts.php
@@ -3,7 +3,7 @@ function site_scripts() {
   global $wp_styles; // Call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
 
     // Load modernizr files in footer
-    wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr.min.js', array(), '', true );
+    wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr.min.js', array(), '', false );
     
     // Adding Foundation scripts file in the footer
     wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/assets/js/foundation.min.js', array( 'jquery' ), '5.5.3', true );
@@ -15,3 +15,17 @@ function site_scripts() {
     wp_enqueue_script( 'offcanvas-js', get_template_directory_uri() . '/assets/js/vendor/offcanvas.min.js', array( 'jquery' ), '', true );
 }
 add_action('wp_enqueue_scripts', 'site_scripts', 999);
+
+// Defer Plugin Javascripts
+// Defer jQuery Parsing using the HTML5 defer property
+if (!(is_admin() )) {
+    function defer_parsing_of_js ( $url ) {
+        if ( FALSE === strpos( $url, '.js' ) ) return $url;
+        if ( strpos( $url, 'jquery.js' ) ) return $url;
+        if ( strpos( $url, 'modernizr.min.js' ) ) return $url;
+        if ( strpos( $url, 'foundation.min.js' ) ) return $url;
+        // return "$url' defer ";
+        return "$url' defer onload='";
+    }
+    add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
+}
