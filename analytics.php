d0253dcb65a00aec72d53af0fce898a8a7a18caa
ksascomm <ksasweb@jhu.edu>
2016-10-27 15:52:35 -0400

    minify analytics

--- analytics.php
+++ analytics.php
@@ -1,59 +1,9 @@
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-40512757-1', 'jhu.edu');
-  ga('send', 'pageview');
-
+!function(a,b,c,d,e,f,g){a.GoogleAnalyticsObject=e,a[e]=a[e]||function(){(a[e].q=a[e].q||[]).push(arguments)},a[e].l=1*new Date,f=b.createElement(c),g=b.getElementsByTagName(c)[0],f.async=1,f.src=d,g.parentNode.insertBefore(f,g)}(window,document,"script","//www.google-analytics.com/analytics.js","ga"),ga("create","UA-40512757-1","jhu.edu"),ga("send","pageview");
 </script>
-
 <script type="text/javascript">
-/*<![CDATA[*/
-(function() {
-var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
-sz.src = '//siteimproveanalytics.com/js/siteanalyze_11464.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
-})();
-/*]]>*/
+!function(){var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="//siteimproveanalytics.com/js/siteanalyze_11464.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)}();
 </script>
-
 <script>
-function _gaLt(event){
-    var el = event.srcElement || event.target;
-
-    /* Loop up the DOM tree through parent elements if clicked element is not a link (eg: an image inside a link) */
-    while(el && (typeof el.tagName == 'undefined' || el.tagName.toLowerCase() != 'a' || !el.href)){
-        el = el.parentNode;
-    }
-
-    if(el && el.href){
-        /* link */
-        var link = el.href;
-        if(link.indexOf(location.host) == -1 && !link.match(/^javascript\:/i)){ /* external link */
-            /* HitCallback function to either open link in either same or new window */
-            var hitBack = function(link, target){
-                target ? window.open(link, target) : window.location.href = link;
-            };
-            /* Is target set and not _(self|parent|top)? */
-            var target = (el.target && !el.target.match(/^_(self|parent|top)$/i)) ? el.target : false;
-            /* send event with callback */
-            ga(
-                "send", "event", "Outgoing Links", link,
-                document.location.pathname + document.location.search,
-                {"hitCallback": hitBack(link, target)}
-            );
-
-            /* Prevent standard click */
-            event.preventDefault ? event.preventDefault() : event.returnValue = !1;
-        }
-
-    }
-}
-
-/* Attach the event to all clicks in the document after page has loaded */
-var w = window;
-w.addEventListener ? w.addEventListener("load",function(){document.body.addEventListener("click",_gaLt,!1)},!1)
- : w.attachEvent && w.attachEvent("onload",function(){document.body.attachEvent("onclick",_gaLt)});
+function _gaLt(a){for(var b=a.srcElement||a.target;b&&("undefined"==typeof b.tagName||"a"!=b.tagName.toLowerCase()||!b.href);)b=b.parentNode;if(b&&b.href){var c=b.href;if(c.indexOf(location.host)==-1&&!c.match(/^javascript\:/i)){var d=function(a,b){b?window.open(a,b):window.location.href=a},e=!(!b.target||b.target.match(/^_(self|parent|top)$/i))&&b.target;ga("send","event","Outgoing Links",c,document.location.pathname+document.location.search,{hitCallback:d(c,e)}),a.preventDefault?a.preventDefault():a.returnValue=!1}}}var w=window;w.addEventListener?w.addEventListener("load",function(){document.body.addEventListener("click",_gaLt,!1)},!1):w.attachEvent&&w.attachEvent("onload",function(){document.body.attachEvent("onclick",_gaLt)});
 </script>
