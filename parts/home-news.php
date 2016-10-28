8fb07a4a6b5c58bcc40c96b6a8d2dd8626000a57
ksascomm <ksasweb@jhu.edu>
2016-10-28 15:20:01 -0400

    fixed non-distinguishable landmarks

--- parts/home-news.php
+++ parts/home-news.php
@@ -1,5 +1,5 @@
-<article class="small-12 medium-4 columns">
-	<a href="<?php the_permalink(); ?>">
+<article class="small-12 medium-4 columns" aria-label="<?php the_title();?> article" id="post-<?php the_ID(); ?>">
+	<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 		<?php the_post_thumbnail('rss'); ?>
 		<h3><?php the_title(); ?></h3>
 		<summary><?php the_excerpt(); ?></summary>
